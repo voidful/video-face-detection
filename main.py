ae9f5b36195d744c9734ff70f66b716144dd35e1
Eric Lam <voidful.stack@gmail.com>
2024-10-10 00:28:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,10 @@ from scipy.cluster.hierarchy import linkage, fcluster
 from scipy.spatial.distance import pdist
 from collections import Counter
 
-
 TOLERANCE = 0.7
 LOG_CLUSTER_IMG = False
-BATCH_SIZE = 30
-
-# def find_face_in_pool(face_emb, emb_pool, TOLERANCE=0.4):
-#     """ 
-#     Return the pos of the first match, otherwise return -1.
-#     """
-#     distances = list(face_recognition.face_distance(emb_pool, face_emb))
-#     distances = [dis if dis <= TOLERANCE else 1 for dis in distances]
-#     if not distances or min(distances) == 1:
-#         return -1
-#     return np.argmin(distances)
-
+BATCH_SIZE = 64 
+NUM_JITTERS = 1 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -50,29 +39,27 @@ if __name__ == "__main__":
         # Find face locations with GPU
         batch_of_face_locations = []
         for batch_i in range(0, len(batch_img), BATCH_SIZE):
-            end = min(len(batch_img), batch_i+BATCH_SIZE)    
+            end = min(len(batch_img), batch_i + BATCH_SIZE)    
             locations = face_recognition.batch_face_locations(batch_img[batch_i:end], number_of_times_to_upsample=0)
             batch_of_face_locations.extend(locations)
 
-        # Encode the images into embedding
+        # Encode the images into embeddings
         for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
-            face_areas = [(pos[2]-pos[0])*(pos[1]-pos[3]) for pos in face_locations]
+            face_areas = [(pos[2] - pos[0]) * (pos[1] - pos[3]) for pos in face_locations]
             face_areas = sorted(face_areas, reverse=True)
 
             # Background faces removal
-            # Definition: the face area is more than 3x smaller than
-            # the smallest face in the detected faces.
             end_idx = len(face_areas)
             for i in range(len(face_areas)):
-                if i+1 < len(face_areas) and face_areas[i] / face_areas[i+1] > 3:
-                    end_idx = i+1
+                if i + 1 < len(face_areas) and face_areas[i] / face_areas[i + 1] > 3:
+                    end_idx = i + 1
                     break
             face_areas = face_areas[:end_idx]
 
             # Encode faces into embeddings
             face_embs = face_recognition.face_encodings(
-                batch_img[frame_number_in_batch], known_face_locations=face_locations, num_jitters=10,
-                model='large'
+                batch_img[frame_number_in_batch], known_face_locations=face_locations,
+                num_jitters=NUM_JITTERS, model='large'
             )
             if len(face_areas) > 0:
                 face_in_frames.append(len(face_areas))
@@ -89,9 +76,8 @@ if __name__ == "__main__":
         if len(all_embs) == 0:
             results.append([video_folder, 0, []])
 
-        # Only one face is found in only one frame
         elif len(all_embs) == 1:
-            results.append([video_folder, has_face/N, [1.0]])
+            results.append([video_folder, has_face / N, [1.0]])
 
         else:
             # Run clustering
@@ -102,10 +88,11 @@ if __name__ == "__main__":
             counter = Counter(clusters)
             order_id = sorted(counter, key=counter.get, reverse=True)
 
-            results.append([ # folder_name, % frames with face, face cluster sizes
+            results.append([
                 video_folder,
-                round(has_face/N, 2),
-                [round(counter[i]/N, 2) for i in order_id], np.mean(face_in_frames)
+                round(has_face / N, 2),
+                [round(counter[i] / N, 2) for i in order_id],
+                np.mean(face_in_frames)
             ])
         
         if LOG_CLUSTER_IMG:
@@ -114,7 +101,5 @@ if __name__ == "__main__":
                 os.makedirs(f"debug/{cluster}/", exist_ok=True)
                 cv2.imwrite(f"debug/{cluster}/{i}.png", all_faces[i])
 
-        
         with open('results.json', "w") as fout:
             json.dump(results, fout, indent=2, ensure_ascii=False)
-
