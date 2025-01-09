fe505d0c09daf3481a5d5ca57341c23490a55fe7
voidful <voidful.stack@gmail.com>
2025-01-10 00:14:43 +0800

    refactor(utils): replace regex id extraction with hash-based id

--- utils.py
+++ utils.py
@@ -1,9 +1,6 @@
-import re
+import hashlib
 
-id_reg = re.compile(r'(\d+)(?=\.\w+$)|\[(.*?)\](?!.*\[[^\[\]]*\])')
 
 def extract_id(filename):
-    match = re.search(id_reg, filename)
-    if not match:
-        raise ValueError('ID unfetchable')
-    return match.group(1) if match.group(1) else match.group(2)
+    # use hash function to create unique id of filename
+    return hashlib.md5(filename.encode()).hexdigest()[:10]
